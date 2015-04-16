c42c10167bf50b98f62d17a70111886a91f658b6
Rohit Kumar <roxtar@gmail.com>
2015-04-16 01:09:40 +0000

    Update test_prof.go to a new program

--- profiling/test_prof.go
+++ profiling/test_prof.go
@@ -1,18 +1,45 @@
 package main
 
 import "runtime/pprof"
+import "runtime"
 import "log"
 import "os"
 import "flag"
+import "fmt"
 
-func factorial(n int) int {
-	if n == 0 {
-		return 1
+// Another version of map reduce which doesn't use
+// any arrays. Everything is done through channels.
+//
+// Is much more elegant than pmapreduce.go, but
+// also much slower.
+
+func f(x int64) int64 {
+	return x * x
+}
+
+func do_map(in chan int64, out chan int64) {
+	for i := range in {
+		out <- f(i)
 	}
-	return n * factorial(n-1)
+	close(out)
+}
+
+func do_reduce(in chan int64, out chan int64) {
+	var total int64 = 0
+	for i := range in {
+		total += i
+	}
+	out <- total
+	close(out)
 }
 
 func main() {
+
+	const n = 3000000
+	const nprocs = 4
+
+	runtime.GOMAXPROCS(nprocs)
+
 	var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 	flag.Parse()
 	if *cpuprofile == "" {
@@ -23,9 +50,19 @@ func main() {
 		log.Fatal(err)
 	}
 	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
-	for i := 0; i < 100; i++ {
-		factorial(7)
-	}
 
+	map_in := make(chan int64, n)
+	map_out := make(chan int64, n)
+	reduce_out := make(chan int64, n)
+
+	go do_map(map_in, map_out)
+	go do_reduce(map_out, reduce_out)
+
+	for i := 0; i < n; i++ {
+		map_in <- int64(i)
+	}
+	close(map_in)
+	fmt.Printf("%v \n", <-reduce_out)
+	pprof.StopCPUProfile()
+	f.Close()
 }
